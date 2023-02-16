fd0d5c99bf8e090eaf5005b2a88ba263cf46d912
paras11917 <70134657+paras11917@users.noreply.github.com>
2023-02-17 00:03:08 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/7afNEODEaEH64Mzxc-C_ye4K1vBiXV2V",
-      accounts: ['15658d88c4a04708deda10f164e3869d3f2e671f64353984eb3e20d0c6db6cb1'],
+      url: "alchemy key url",
+      accounts: ['accountkey'],
     },
   },
 };
